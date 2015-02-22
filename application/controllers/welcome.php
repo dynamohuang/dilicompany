ce4eb2dba842a1ba2903c276a03dee6bdf9d775c
dinglehuang <huangdingzhi1993@gmail.com>
2015-02-22 17:50:44 +0800

    完成联系我们和关于我们子页面

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -6,6 +6,7 @@ class Welcome extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->smarty->assign('base_url',base_url());
+		$this->smarty->assign('site_url',site_url());
 	}
 	public function index()
 	{
@@ -32,7 +33,19 @@ class Welcome extends CI_Controller {
 	}
 	public function intro(){
 		
-
+		//main_data
+		$this->db->select('title,content');
+		$this->db->where('key','intro');
+		$query = $this->db->get("dili_u_m_cate_content",1,0);
+		$intro = $query->result();
+		$this->smarty->assign('main_data',$intro[0]);
+		
+		//联系我们
+		$this->db->select('title,content');
+		$this->db->where('key','contact_us');
+		$query = $this->db->get("dili_u_m_page_data",1,0);
+		$contact_us = $query->result();
+		$this->smarty->assign('contact_us',$contact_us[0]);
 		$this->smarty->view('common.html');
 		
 	}
@@ -47,8 +60,39 @@ class Welcome extends CI_Controller {
 		
 	}
 	public function join(){
+		//main_data
+		$this->db->select('title,content');
+		$this->db->where('key','join');
+		$query = $this->db->get("dili_u_m_cate_content",1,0);
+		$join = $query->result();
+		$this->smarty->assign('main_data',$join[0]);
+		
+		//联系我们
+		$this->db->select('title,content');
+		$this->db->where('key','contact_us');
+		$query = $this->db->get("dili_u_m_page_data",1,0);
+		$contact_us = $query->result();
+		$this->smarty->assign('contact_us',$contact_us[0]);
+		$this->smarty->view('common.html');
 		
 	} 
+	public function contact(){
+		//main_data
+		$this->db->select('title,content');
+		$this->db->where('key','contact');
+		$query = $this->db->get("dili_u_m_cate_content",1,0);
+		$contact = $query->result();
+		$this->smarty->assign('main_data',$contact[0]);
+		
+		//联系我们
+		$this->db->select('title,content');
+		$this->db->where('key','contact_us');
+		$query = $this->db->get("dili_u_m_page_data",1,0);
+		$contact_us = $query->result();
+		$this->smarty->assign('contact_us',$contact_us[0]);
+		$this->smarty->view('common.html');
+		
+	}
 }
 
 /* End of file welcome.php */
