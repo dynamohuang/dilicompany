056ee8ac3e11c50c2d27de05eb94b2cc9444d7cb
dinglehuang <huangdingzhi1993@gmail.com>
2015-02-22 17:51:55 +0800

    数据同步

--- dilicompany.sql
+++ dilicompany.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50621
 File Encoding         : 65001
 
-Date: 2015-02-22 14:16:48
+Date: 2015-02-22 17:51:06
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -35,7 +35,7 @@ CREATE TABLE `dili_admins` (
 -- ----------------------------
 -- Records of dili_admins
 -- ----------------------------
-INSERT INTO `dili_admins` VALUES ('1', 'dilicompany ', '9f9f21c29d03adb1c62889c2595d35ddd260fb34', '04ca8e3332', 'hello@dilicms.com', '1', '1');
+INSERT INTO `dili_admins` VALUES ('1', 'dilicompany ', '6a69f3310fc8ea955902b6a8898668ed579ce82d', '04ca8e3332', 'hello@dilicms.com', '1', '1');
 
 -- ----------------------------
 -- Table structure for `dili_attachments`
@@ -133,7 +133,6 @@ CREATE TABLE `dili_cate_models` (
 -- ----------------------------
 -- Records of dili_cate_models
 -- ----------------------------
-INSERT INTO `dili_cate_models` VALUES ('1', 'common', '通用页面内容', '5', '1', '0', '0', '0', null);
 INSERT INTO `dili_cate_models` VALUES ('3', 'article_type', '文章分类', '20', '1', '0', '0', '0', null);
 
 -- ----------------------------
@@ -220,13 +219,14 @@ CREATE TABLE `dili_models` (
   `thumb_preferences` text,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of dili_models
 -- ----------------------------
-INSERT INTO `dili_models` VALUES ('3', 'page_data', '基本页面内容配置', '5', '0', '0', null);
+INSERT INTO `dili_models` VALUES ('3', 'page_data', '页面内容配置', '5', '0', '0', null);
 INSERT INTO `dili_models` VALUES ('2', 'article', '文章', '10', '1', '0', '{\"enabled\":[],\"default\":\"original\"}');
+INSERT INTO `dili_models` VALUES ('4', 'cate_content', '一级栏目内容', '10', '0', '0', null);
 
 -- ----------------------------
 -- Table structure for `dili_model_fields`
@@ -251,7 +251,7 @@ CREATE TABLE `dili_model_fields` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`,`model`),
   KEY `model` (`model`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of dili_model_fields
@@ -263,6 +263,9 @@ INSERT INTO `dili_model_fields` VALUES ('4', 'article', '内容', '2', 'wysiwyg'
 INSERT INTO `dili_model_fields` VALUES ('5', 'maker', '发布者', '2', 'input', '0', '', '0', '0', '', '', '1', '1', '0', '1');
 INSERT INTO `dili_model_fields` VALUES ('7', 'type', '文章类别', '2', 'select_from_model', '0', 'article_type|type_name', '0', '0', 'required', '', '1', '1', '0', '1');
 INSERT INTO `dili_model_fields` VALUES ('10', 'key', 'key_name', '3', 'input', '0', '', '0', '0', 'required', '', '0', '1', '0', '1');
+INSERT INTO `dili_model_fields` VALUES ('11', 'key', 'key_name', '4', 'input', '0', '', '0', '0', 'required', '', '1', '1', '0', '1');
+INSERT INTO `dili_model_fields` VALUES ('12', 'title', '标题', '4', 'input', '0', '', '0', '0', 'required', '', '1', '1', '0', '1');
+INSERT INTO `dili_model_fields` VALUES ('13', 'content', '内容', '4', 'wysiwyg', '0', '', '0', '0', '', '', '0', '0', '0', '1');
 
 -- ----------------------------
 -- Table structure for `dili_plugins`
@@ -393,8 +396,7 @@ CREATE TABLE `dili_sessions` (
 -- ----------------------------
 -- Records of dili_sessions
 -- ----------------------------
-INSERT INTO `dili_sessions` VALUES ('d61ed3e2e74aa88820f592930068821d', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36', '1424450675', '');
-INSERT INTO `dili_sessions` VALUES ('031b929e6a019769468734a2da826d77', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36', '1424585597', 'a:3:{s:9:\"user_data\";s:0:\"\";s:21:\"throttles_dilicompany\";i:1;s:3:\"uid\";s:1:\"1\";}');
+INSERT INTO `dili_sessions` VALUES ('422c3781848b3ed2ea09cecb156b32a6', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36', '1424597137', 'a:5:{s:9:\"user_data\";s:0:\"\";s:21:\"throttles_dilicompany\";i:0;s:3:\"uid\";s:1:\"1\";s:10:\"model_type\";s:5:\"model\";s:5:\"model\";s:12:\"cate_content\";}');
 
 -- ----------------------------
 -- Table structure for `dili_site_settings`
@@ -437,7 +439,7 @@ CREATE TABLE `dili_throttles` (
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of dili_throttles
@@ -454,7 +456,7 @@ CREATE TABLE `dili_u_c_article_type` (
   `path` varchar(50) DEFAULT '',
   `type_name` varchar(50) NOT NULL DEFAULT '',
   PRIMARY KEY (`classid`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of dili_u_c_article_type
@@ -462,22 +464,6 @@ CREATE TABLE `dili_u_c_article_type` (
 INSERT INTO `dili_u_c_article_type` VALUES ('1', '0', '1', '{0}', '新闻中心');
 INSERT INTO `dili_u_c_article_type` VALUES ('2', '0', '1', '{0}', '产品中心');
 INSERT INTO `dili_u_c_article_type` VALUES ('3', '0', '1', '{0}', '经典案列');
-
--- ----------------------------
--- Table structure for `dili_u_c_common`
--- ----------------------------
-DROP TABLE IF EXISTS `dili_u_c_common`;
-CREATE TABLE `dili_u_c_common` (
-  `classid` int(5) unsigned NOT NULL AUTO_INCREMENT,
-  `parentid` int(5) unsigned NOT NULL DEFAULT '0',
-  `level` int(2) unsigned NOT NULL DEFAULT '1',
-  `path` varchar(50) DEFAULT '',
-  PRIMARY KEY (`classid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of dili_u_c_common
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for `dili_u_m_article`
@@ -503,7 +489,30 @@ INSERT INTO `dili_u_m_article` VALUES ('1', '1424073882', '1424073882', '1', '1'
 INSERT INTO `dili_u_m_article` VALUES ('2', '1424073907', '1424073907', '1', '1', '亚洲新能源大会召开', 'sdfdsfdsfasdgafgasgds', '', '1');
 INSERT INTO `dili_u_m_article` VALUES ('3', '1424073929', '1424073929', '1', '1', '热烈祝贺我司美股上市', 'dfdsfsafdsfdsf', '', '1');
 INSERT INTO `dili_u_m_article` VALUES ('4', '1424073950', '1424073950', '1', '1', '互联网营销的成功', 'dsfdsfsdfsdf', '', '1');
-INSERT INTO `dili_u_m_article` VALUES ('5', '1424073972', '1424073972', '1', '1', '2015年第一季度公司销售业绩', 'fdsafasdgbat', 'ddd', '1');
+INSERT INTO `dili_u_m_article` VALUES ('5', '1424073972', '1424595855', '1', '1', '2015年第一季度公司销售业绩', 'fdsafasdgbat', '', '1');
+
+-- ----------------------------
+-- Table structure for `dili_u_m_cate_content`
+-- ----------------------------
+DROP TABLE IF EXISTS `dili_u_m_cate_content`;
+CREATE TABLE `dili_u_m_cate_content` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `create_user` tinyint(10) unsigned NOT NULL DEFAULT '0',
+  `update_user` tinyint(10) unsigned NOT NULL DEFAULT '0',
+  `key` varchar(100) NOT NULL DEFAULT '',
+  `title` varchar(100) NOT NULL DEFAULT '',
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of dili_u_m_cate_content
+-- ----------------------------
+INSERT INTO `dili_u_m_cate_content` VALUES ('1', '1424596733', '1424596733', '1', '1', 'intro', '关于我们', '<p style=\"font-size:14px;font-family:Verdana, Arial, Helvetica, sans-serif;color:#505050;\">\r\n	<strong>重庆泓彬醇基有限公司 &nbsp;</strong>成立于2013年5月，坐落于重庆市万盛区中心广场，主要从事节能环保事业，是集研发、生产和销售于一体的综合性企业。在能源日渐匮乏和大气污染日趋严重的今天，节能环保将是未来世界大主题，我们的目标：做中国的节能燃料领导者。\r\n</p>\r\n<p style=\"font-size:14px;font-family:Verdana, Arial, Helvetica, sans-serif;color:#505050;\">\r\n	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 公司主营产品：“甘氏节能”醇基节能燃料、节能灶具、节能控制器等。 公司研制开发的醇基节能燃料及灶具，适用于酒店、餐厅、家用、夜排档等。这种新型燃料燃烧充分，无烟无味，无残液残渣，火力猛，温度高，比液化气、柴油节省30%左右！燃料呈液态，在常压下灌装使用，消除了液化气罐的高压易爆炸的安全隐患，燃烧无烟无残留使得整个厨房环境更干净卫生。<br />\r\n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 公司产品通过了国家标准，我们还建立了比国家标准更高、要求更严格的行业标准。另外，我们还取得了储存液态燃料的装置的专利证书、“甘氏节能”商标和危化品经营许可证。<br />\r\n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 我司产品节能环保高效的优势得到众多客户的认可，现在成都地区我们已经拥有500多家餐馆、酒店、食堂的客户，月供燃料200余吨。公司还在福州、西昌、兰州、攀枝花、阿坝等地区有加盟商，规模正在不断扩大。\r\n</p>\r\n<p style=\"font-size:14px;font-family:Verdana, Arial, Helvetica, sans-serif;color:#505050;\">\r\n	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 公司强大的技术力量团队将为您提供及时、完善、周到的服务。我们承诺：将在接到您电话通知的24小时内送油上门。如果有任何问题，我们将在接到您通知后48小时内上门解决处理，特殊紧急情况须更快上门服务。售后服务专线：400-666-3729。\r\n</p>\r\n<p style=\"font-size:14px;font-family:Verdana, Arial, Helvetica, sans-serif;color:#505050;\">\r\n	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 我们的宗旨：诚信，创新，利益双赢。\r\n</p>');
+INSERT INTO `dili_u_m_cate_content` VALUES ('2', '1424596859', '1424596859', '1', '1', 'join', '招商加盟', '<strong>重庆泓彬醇基有限公司</strong><span style=\"color:#505050;font-family:Verdana;line-height:24px;background-color:#FFFFFF;\">拥有成熟的技术，可以为您提供更高效的产品。仅仅重庆地区我们就自主开拓了500多家客户，包括大家熟悉的“祖母炖品”、“田园印象”、“厨品楼”、“东海渔港”、“张大胡子片片鱼”、“内江风味菜”、“世外桃源”、“乡村菜馆”、“春回大地”、“钱苑食府”、“自贡好吃客”、“贵州芋儿鸡”等等。每位客户对我公司产品评价都高于同行其他商家。如果您有意向加盟我公司，我们可以带您去实地参观考察，让您能够亲身体验我们的产品，并亲手操作，为您熟悉我们的产品提供更便捷的方式。</span><br />\r\n<span style=\"color:#505050;font-family:Verdana;line-height:24px;background-color:#FFFFFF;\">加盟我公司您无须承担任何风险，总公司会为您提供相关产品以及配货，并参与经营，您只需负责当地销售，送油，调试以及售后服务等工作。公司强大的生产与销售以及完善的售后一条龙服务给您的代理和销售带来强有力地支持！</span><br />\r\n<span style=\"color:#505050;font-family:Verdana;line-height:24px;background-color:#FFFFFF;\">加盟条件：</span><br />\r\n<span style=\"color:#505050;font-family:Verdana;line-height:24px;background-color:#FFFFFF;\">1，&nbsp;有自主销售能力</span><br />\r\n<span style=\"color:#505050;font-family:Verdana;line-height:24px;background-color:#FFFFFF;\">2，&nbsp;要懂得保护公司品牌，维护品牌利益</span><br />\r\n<span style=\"color:#505050;font-family:Verdana;line-height:24px;background-color:#FFFFFF;\">3，&nbsp;良好的销售渠道</span><br />\r\n<span style=\"color:#505050;font-family:Verdana;line-height:24px;background-color:#FFFFFF;\">加盟流程：</span><br />\r\n<span style=\"color:#505050;font-family:Verdana;line-height:24px;background-color:#FFFFFF;\">1，&nbsp;首先需报名参与公司培训，了解公司产品</span><br />\r\n<span style=\"color:#505050;font-family:Verdana;line-height:24px;background-color:#FFFFFF;\">2，&nbsp;有意向加盟者可与我们的工作人员联系，详谈加盟的各项事情。无论您有何疑问，我们都将尽心为您详细解答</span><br />\r\n<span style=\"color:#505050;font-family:Verdana;line-height:24px;background-color:#FFFFFF;\">3，&nbsp;满意者可与公司签订加盟协议，正式加盟我们公司</span>');
+INSERT INTO `dili_u_m_cate_content` VALUES ('3', '1424597137', '1424597137', '1', '1', 'contact', '联系我们', '<strong>重庆泓彬醇基有限公司<br />\r\n</strong><span style=\"color:#505050;font-family:Verdana;font-size:14px;line-height:28px;\">联系人&nbsp;&nbsp; ：毛先生</span><br />\r\n<span style=\"color:#505050;font-family:Verdana;font-size:14px;line-height:28px;\">联系手机：13368088888</span><br />\r\n<span style=\"color:#505050;font-family:Verdana;font-size:14px;line-height:28px;\">服务热线：400-666-3729/023-66133292</span><br />\r\n<span style=\"color:#505050;font-family:Verdana;font-size:14px;line-height:28px;\">企业邮箱：</span><a href=\"mailto:ycjn2014@163.com\">ycjn2014@163.com</a><br />\r\n<p>\r\n	<span style=\"color:#505050;font-family:Verdana;font-size:14px;line-height:28px;\">公司地址：重庆市万盛区中心广场</span>\r\n</p>\r\n<p>\r\n	<span style=\"color:#505050;font-family:Verdana;font-size:14px;line-height:28px;\">乘车路线：南平长途汽车站高速大巴到万盛车站</span>\r\n</p>\r\n<p>\r\n	<span style=\"color:#505050;font-family:Verdana;font-size:14px;line-height:28px;\">驾车路线：高速</span>\r\n</p>');
 
 -- ----------------------------
 -- Table structure for `dili_u_m_page_data`
